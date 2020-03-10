67344fac7cb8eab6ade56abd9c0a9126865509ea
jaspervangent69 <57948820+jaspervangent69@users.noreply.github.com>
2020-03-10 12:40:37 +0100

    Add project files.

---
+++ Cinema2/Form2.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cinema2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 Form1 = new Form1();
+            Form1.Show();
+            this.Close();
+            
+        }
+
+        class JSONData
+        {
+            static void Main(string[] args)
+            {
+
+            }
+        }
+
+
+    }
+}
